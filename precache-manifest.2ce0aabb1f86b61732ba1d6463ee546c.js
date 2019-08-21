defc9a58f7fcd8309a330e33d00312fd63c96248
Artur Shestopalov <Artiikk2@gmail.com>
2019-08-21 14:10:28 +0200

    Updates

---
+++ precache-manifest.2ce0aabb1f86b61732ba1d6463ee546c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f506635280c0405ce7bb882831878a9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bf82a8b3b177862052b",
+    "url": "/static/css/main.0b80be12.chunk.css"
+  },
+  {
+    "revision": "71e9684ee4e9321794f5",
+    "url": "/static/js/2.a9aba5ce.chunk.js"
+  },
+  {
+    "revision": "0bf82a8b3b177862052b",
+    "url": "/static/js/main.bf22775c.chunk.js"
+  },
+  {
+    "revision": "2255df240c3f5cc50c58",
+    "url": "/static/js/runtime~main.f9395578.js"
+  }
+]);
